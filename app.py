822cdb93985814b04df36851afeae1854d45dc6f
Nimet Ozkan <melaht2016@icloud.com>
2022-05-14 20:07:02 -0500

    added new functions

--- app.py
+++ app.py
@@ -7,13 +7,27 @@ app.secret_key = "CooKIEmONSter123xy0&!#usL*txGha_bsnSb72shjkshME42"
 
 @app.route("/")
 def index():
-    return "<h1>This is My Starter App</h1>"
+    return "SHOPIFY BACKEND CHALLENGE"
 
+@app.route("/view")
+def get_list_of_inventories():
+    all_items = get_db()
+    print("View a list of Inventory Items")
+    # returning a python list to view all the inventory items in our database
+    return str(all_items)
+
+# an helper function to be able to access the raw data of our database
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect('inventory_list.db')
-    return db
+        cursor = db.cursor()
+        # groceries is the table name of our inventory_list database
+        cursor.execute("select name from groceries")
+        all_data = cursor.fetchall()
+        # only get the string portion of the values in the database
+        all_data = [str(val[0]) for val in all_data]
+    return all_data
 
 @app.teardown_appcontext
 def close_connection(exception):
