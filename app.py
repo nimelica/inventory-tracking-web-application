480e9d136881e391e1f83affcf03fb0fece0022e
Nimet Ozkan <melaht2016@icloud.com>
2022-05-17 21:33:27 -0500

    edited

--- app.py
+++ app.py
@@ -37,57 +37,6 @@ class Inventory_Manager:
             return e
         db.close()
 
-    # when deleting an item, allow deletion comments
-    def deletion_message(self):
-        com_db = sqlite3.connect('comments.db')
-        com_cursor = com_db.cursor()
-        user_name = input('Please enter your name: ' )
-        message = input('You can enter your comment: ')
-        command = "INSERT INTO Comment (user_name, message) VALUES (?, ?);"
-        input_db = (user_name, message)
-        try:
-            com_cursor.execute(command, input_db)
-            com_db.commit()
-        except Exception as e:
-            com_db.rollback()
-            return e
-        com_db.close()
-
-    # when deleting an item, allow undeletion of a comment 
-    def delete_comment(self):
-        user_name = input('Please enter your name: ' )
-        command = "DELETE FROM Comment WHERE user_name = {}".format(user_name)
-        com_db = sqlite3.connect('comments.db')
-        com_cursor = com_db.cursor()
-        try:
-            com_cursor.execute(command)
-            com_db.commit()
-            print('Your comment is deleted!')
-        except Exception as e:
-            com_db.rollback()
-            return e
-        com_db.close()
-
-    # delete an item
-    def delete_item(self, name):
-        command = "DELETE FROM Item WHERE name = ?"
-        db = sqlite3.connect('databases/products.db')
-        cursor = db.cursor()
-        try:
-            cursor.execute(command, (name,))
-            db.commit()
-            print("record deleted successfully")
-            prompt = input('Do you want to leave a message Y/N: ')
-            if prompt == 'N':
-                return ("Item deleted successfully!")
-            elif prompt == 'Y':
-                self.deletion_message()
-        except Exception as e:
-            print("error in operation")
-            db.rollback()
-            return e
-        db.close()
-
     # view the list of item names
     def view_item_names(self):
         # returning a python list to view all the inventory items in our database
@@ -104,6 +53,21 @@ class Inventory_Manager:
         for data in all_data:
             print(data)
 
+    # delete an item
+    def delete_item(self, name):
+        command = "DELETE FROM Item WHERE name = ?"
+        db = sqlite3.connect('databases/products.db')
+        cursor = db.cursor()
+        try:
+            cursor.execute(command, (name,))
+            db.commit()
+            print("record deleted successfully")
+        except Exception as e:
+            print("error in operation")
+            db.rollback()
+            return e
+        db.close()
+
     # view the list of the items in details
     def view_all_items(self):
         db = sqlite3.connect('databases/products.db')
@@ -114,6 +78,38 @@ class Inventory_Manager:
         except Exception as e:
             return e
         return result
+    
+    # when deleting an item, allow deletion comments
+    def add_comment(self):
+        com_db = sqlite3.connect('comments.db')
+        com_cursor = com_db.cursor()
+        user_name = input('Please enter your name: ' )
+        message = input('You can enter your comment: ')
+        command = "INSERT INTO Comment (user_name, message) VALUES (?, ?);"
+        input_db = (user_name, message)
+        try:
+            com_cursor.execute(command, input_db)
+            com_db.commit()
+        except Exception as e:
+            com_db.rollback()
+            return e
+        com_db.close()
+
+    # when deleting an item, allow undeletion of a comment 
+    def uncomment(self):
+        user_name = input('Please enter your name: ' )
+        command = "DELETE FROM Comment WHERE user_name = ?"
+        com_db = sqlite3.connect('comments.db')
+        com_cursor = com_db.cursor()
+        try:
+            com_cursor.execute(command, user_name)
+            com_db.commit()
+            print('Your comment is deleted!')
+        except Exception as e:
+            com_db.rollback()
+            return e
+        com_db.close()
+
 
 # test the program
 def main():
@@ -122,9 +118,7 @@ def main():
    im.view_item_names()
    # print(im.view_all_items())
    # im.delete_item('Nerds')
-   # im.view_item_names()
-   im.deletion_message()
-
+   im.add_message
 
 if __name__ == "__main__":
     main()
